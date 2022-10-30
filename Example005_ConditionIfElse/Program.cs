1851a535203efa9501f8e54e9cc6c3125bf55211
«Dmitrii <cradle1981@gmail.com>
2022-10-30 16:17:29 +0500

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользоваателя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dmitrii")
+{
+    Console.WriteLine("Wow, it's Dmitrii!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
